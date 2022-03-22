20a91bff4ca500acf271c40b30e3a8ba8bc2d5d1
sams4566 <sams4566@gmail.com>
2022-03-22 14:07:24 +0000

    Reduce the size of employee records

--- src/main/java/com/sparta/ss/fileio/FileIOUtils.java
+++ src/main/java/com/sparta/ss/fileio/FileIOUtils.java
@@ -13,7 +13,7 @@ public class FileIOUtils {
             bufferedReader.readLine();
             String line = bufferedReader.readLine();
             FileWriter writer = new FileWriter("src/main/java/com/sparta/ss/fileio/ValidEntries.csv");
-//            FileWriter corruptedWriter = new FileWriter("src/main/java/com/sparta/ss/fileio/CorruptedEntries.csv");
+            FileWriter corruptedWriter = new FileWriter("src/main/java/com/sparta/ss/fileio/CorruptedEntries.csv");
             while (line != null) {
                 String[] employeeData = line.split(",");
                 EmployeeDTO employeeDTO = new EmployeeDTO(
@@ -28,49 +28,48 @@ public class FileIOUtils {
                         employeeData[8],
                         employeeData[9]
                 );
-//                FileWriter writer = new FileWriter("src/main/java/com/sparta/ss/fileio/ValidEntries.csv");
                 System.out.println(employeeDTO);
-                line = bufferedReader.readLine();
+//                line = bufferedReader.readLine();
                 writer.write(String.valueOf(employeeDTO));
                 writer.write("\n");
-//                FileReader validReader = new FileReader(fileName);
-//                BufferedReader validBufferedReader = new BufferedReader(validReader);
-//                validBufferedReader.readLine();
-//                String validLine = validBufferedReader.readLine();
-//                int counter = 0;
-//                while (validLine != null) {
-//                    String[] validEmployeeData = validLine.split(",");
-//                    EmployeeDTO validEmployeeDTO = new EmployeeDTO(
-//                            validEmployeeData[0],
-//                            validEmployeeData[1],
-//                            validEmployeeData[2],
-//                            validEmployeeData[3],
-//                            validEmployeeData[4],
-//                            validEmployeeData[5],
-//                            validEmployeeData[6],
-//                            validEmployeeData[7],
-//                            validEmployeeData[8],
-//                            validEmployeeData[9]
-//                    );
-////
-//                    if (employeeDTO.getEmpID() == validEmployeeDTO.getEmpID()) {
-//                        System.out.println("uncorrupted");
-//                        System.out.println(employeeDTO.getEmpID());
-//                        counter++;
-//                        System.out.println(counter);
-//                        if (counter > 1) {
-//                            System.out.println(String.valueOf(employeeDTO));
-//                            corruptedWriter.write(String.valueOf(employeeDTO));
-//                            corruptedWriter.write("\n");
-//                            System.out.println("corrupted er gerw gwreg er greg werg ergregre werg wreg erg erg reg werg ewg ");
-//                        }
-//                    } else {
-//                        System.out.println("hello");
-//                    }
-////                    System.out.println(employeeDTO);
-//                    validLine = validBufferedReader.readLine();
-////                    line = bufferedReader.readLine();
-//                }
+                FileReader validReader = new FileReader(fileName);
+                BufferedReader validBufferedReader = new BufferedReader(validReader);
+                validBufferedReader.readLine();
+                String validLine = validBufferedReader.readLine();
+                int counter = 0;
+                while (validLine != null) {
+                    String[] validEmployeeData = validLine.split(",");
+                    EmployeeDTO validEmployeeDTO = new EmployeeDTO(
+                            validEmployeeData[0],
+                            validEmployeeData[1],
+                            validEmployeeData[2],
+                            validEmployeeData[3],
+                            validEmployeeData[4],
+                            validEmployeeData[5],
+                            validEmployeeData[6],
+                            validEmployeeData[7],
+                            validEmployeeData[8],
+                            validEmployeeData[9]
+                    );
+                    if (employeeDTO.getEmpID() == validEmployeeDTO.getEmpID()) {
+                        System.out.println("uncorrupted");
+                        System.out.println(employeeDTO.getEmpID());
+                        counter++;
+                        System.out.println(counter);
+                        if (counter > 1) {
+                            System.out.println(String.valueOf(employeeDTO));
+                            corruptedWriter.write(String.valueOf(employeeDTO));
+                            corruptedWriter.write("\n");
+                            validLine = validBufferedReader.readLine();
+                            System.out.println("corrupted er gerw gwreg er greg werg ergregre werg wreg erg erg reg werg ewg ");
+                        } else {
+                            validLine = validBufferedReader.readLine();
+                        }
+                    } else {
+                        validLine = validBufferedReader.readLine();
+                    }
+                }
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
