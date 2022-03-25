8e57f338b30c8c99075c5f6169b7331b4d94c2b7
mujm4tic <mmoosavi@hotmail.co.uk>
2022-03-25 13:57:28 +0000

    Test 2 additions

--- src/test/java/com/sparta/ss/tests/Test2.java
+++ src/test/java/com/sparta/ss/tests/Test2.java
@@ -1,53 +1,37 @@
 package com.sparta.ss.tests;
 
-import com.mysql.cj.conf.StringPropertyDefinition;
 import com.sparta.ss.fileio.EmployeeDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-import static com.sparta.ss.fileio.CorruptionChecking.checkEmployee;
 import static com.sparta.ss.fileio.CorruptionChecking.idDuplicate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Test2 {
-    @Test
-    @DisplayName("Test checkEmployee")
-//    void testCheckEmployee() {
-//        EmployeeDTO employee1 = new EmployeeDTO("1313" "Mr")
-//        Assertions.assertTrue(checkEmployee(), );
-    }
+
 
     @Test
-    @DisplayName("Test idDuplicate")
-    void testIDDuplicate() {
-        String[] employee = new String[]{"123","Mr", "Mujtaba", "M", "Moosavi", "mm@hotmail.com", "12/12/2012", "1/12/2020", "23128"};
-        EmployeeDTO employeeDTO  = new EmployeeDTO(employee);
-        String[] employee2 = new String[]{"123", "Mr", "Sam", "M", "Moosavi", "sam@hotmail.com", "12/1/2012", "4/12/2020", "23128"};
+    @DisplayName("Test ")
+    void methodName() {
+        Assertions.assertEquals(1,1);
+    }
+    
+    @Test
+    @DisplayName("Test idDuplicate returns true if no duplicate ID")
+    void testIDDuplicateReturnsTrueForNoDuplicates() {
+        String[] employee = new String[]{"123456", "Mr.", "Mujtaba", "M", "Moosavi", "M", "mm@hotmail.com", "12/12/2012", "1/12/2020", "23126"};
+        String[] employee2 = new String[]{"123457", "Mr.", "Sam", "M", "Moosavi", "M", "sam@hotmail.com", "12/1/2012", "4/12/2020", "23128"};
+        String[] employee3 = new String[]{"123458", "Mr.", "Nugat", "M", "Moosavi", "M", "nugat@hotmail.com", "12/1/2008", "4/12/2020", "23132"};
+        EmployeeDTO employeeDTO = new EmployeeDTO(employee);
         EmployeeDTO employeeDTO2 = new EmployeeDTO(employee2);
-        ArrayList<EmployeeDTO> validEmployees = new ArrayList<EmployeeDTO>();
-        validEmployees = {employeeDTO,employeeDTO2};
-
-        Assertions.assertTrue(idDuplicate(employee2));
+        EmployeeDTO employeeDTO3 = new EmployeeDTO(employee3);
+        ArrayList<EmployeeDTO> validEmployees = new ArrayList<>();
+        validEmployees.add(employeeDTO);
+        validEmployees.add(employeeDTO2);
+        idDuplicate(employeeDTO3);
+        Assertions.assertFalse(idDuplicate(employeeDTO3));
     }
-
-
-//    String[] employee = new String[]{"123","Mr", "Mujtaba", "M", "Moosavi", "mm@hotmail.com", "12/12/2012", "1/12/2020", "23128"};
-//    EmployeeDTO employeeDTO  = new EmployeeDTO(employee);
-//    Strin
-//    ArrayList<EmployeeDTO> validEmployees = new ArrayList<EmployeeDTO>();
-//    validEmployees = {123, "Mr", "Mujtaba", "M", "Moosavi", "mm@hotmail.com", 12/12/2012, 01/12/2020, 23128};
-//    EmployeeDTO employee2 = new EmployeeDTO(123, "Mr", "Sam", "M", "Moosavi"
-//            "sam@hotmail.com", 12/2012, 4/12/2020, 23128 );
-//        Assertions.assertTrue(idDuplicate(employee2));
-//
-//    private int empID;
-//    private String namePrefix;
-//    private String firstName;
-//    private String middleInitial;
-//    private String lastName;
-//    private String gender;
-//    private String email;
-//    private LocalDate dob;
-//    private LocalDate dateOfJoining;
-//    private int salary;
 }
+
+
